359673671dedb068187e654afc5b3290728cd81f
Pamela McA'Nulty <pamela@webreply.com>
2013-11-08 12:45:18 -0500

    extract get_message_ctime to handle bad tz info and add --creds= argument to allow credentials from a file
    Signed-off-by: Pamela McA'Nulty <pamela@webreply.com>

--- dobackup.py
+++ dobackup.py
@@ -1,4 +1,4 @@
-import imaplib, os, getpass, re, email, email.utils, datetime, time, calendar
+import imaplib, os, getpass, re, email, email.utils, datetime, time, calendar, sys
 
 uidre = re.compile(r"\d+\s+\(UID (\d+)\)$")
 def getUIDForMessage(n):
@@ -9,6 +9,24 @@ def getUIDForMessage(n):
 	return m.group(1)
 	
 
+def get_message_ctime(d):
+	dt_src = email.utils.parsedate_tz(d)
+	if not dt_src:
+		return None
+	if not dt_src[-1]:
+		# TZ INFO IS MESSY
+		if " --" in d:
+			d = d.replace(" --", " -")
+			dt_src = email.utils.parsedate_tz(d)
+	dt = datetime.datetime(*dt_src[:6])
+	if dt_src[-1]:
+		dt = dt-datetime.timedelta(seconds= dt_src[-1])
+	dt = datetime.datetime.fromtimestamp(calendar.timegm(dt.timetuple()))
+	message_ctime = time.mktime(dt.timetuple())
+	return message_ctime
+	
+
+
 def downloadMessage(n, fname):
 	resp, lst = svr.fetch(n, '(RFC822)')
 	if resp!='OK':
@@ -17,13 +35,10 @@ def downloadMessage(n, fname):
 	f.write(lst[0][1])
 	f.close()
 	message = email.message_from_string(lst[0][1])
-	dt_src = email.utils.parsedate_tz(message['date'])
-	if dt_src:
-		utc_dt = datetime.datetime(*dt_src[:6])-datetime.timedelta(seconds= dt_src[-1])
-		dt = datetime.datetime.fromtimestamp(calendar.timegm(utc_dt.timetuple()))
-		message_ctime = time.mktime(dt.timetuple())
+	message_ctime = get_message_ctime(message['date'])
+	if message_ctime:
 		os.utime(fname, (message_ctime, message_ctime))
-
+	
 
 filere = re.compile(r"(\d+).eml$")
 def UIDFromFilename(fname):
@@ -33,16 +48,30 @@ def UIDFromFilename(fname):
 
 
 svr = imaplib.IMAP4_SSL('imap.gmail.com')
-svr.login(raw_input("Gmail address: "), getpass.getpass("Gmail password: "))
+
+def get_credentials():
+	for a in sys.argv[1:]:
+		if a.startswith("--creds="):
+			# --creds= is a file with TWO lines user\npwd
+			with open(a.split("=", 1)[-1]) as f:
+				user, pwd = f.read().strip().split("\n", 1)
+			return user, pwd
+	
+	user = raw_input("Gmail address: ")
+	pwd = getpass.getpass("Gmail password: ")
+	return user, pwd
+	
+user, pwd = get_credentials()
+svr.login(user, pwd)
 
 resp, [countstr] = svr.select("[Gmail]/All Mail", True)
 count = int(countstr)
 
 existing_files = os.listdir(".")
 if existing_files:
-    lastdownloaded = max(UIDFromFilename(f) for f in existing_files)
+	lastdownloaded = max(UIDFromFilename(f) for f in existing_files)
 else:
-    lastdownloaded = 0
+	lastdownloaded = 0
 
 
 # A simple binary search to see where we left off
