2f1868ef8dc12c78ff1bf61da8364d957fdbeba3
Pamela McA'Nulty <pamela@webreply.com>
2013-12-02 08:39:36 -0500

    pep8-ify, except for 3 line-too-longs that are status/error reporting.
    Signed-off-by: Pamela McA'Nulty <pamela@webreply.com>

--- dobackup.py
+++ dobackup.py
@@ -1,102 +1,112 @@
-import imaplib, os, getpass, re, email, email.utils, datetime, time, calendar, sys
+# -*- coding: utf-8 -*-
 
-uidre = re.compile(r"\d+\s+\(UID (\d+)\)$")
+import imaplib
+import os
+import getpass
+import re
+import email
+import email.utils
+import datetime
+import time
+import calendar
+import sys
+
+
+UID_RE = re.compile(r"\d+\s+\(UID (\d+)\)$")
+FILE_RE = re.compile(r"(\d+).eml$")
+GMAIL_FOLDER_NAME = "[Gmail]/All Mail"
+
+
 def getUIDForMessage(svr, n):
-	resp, lst = svr.fetch(n, 'UID')
-	m = uidre.match(lst[0])
-	if not m:
-		raise Exception("Internal error parsing UID response: %s %s.  Please try again" % (resp, lst))
-	return m.group(1)
-	
+    resp, lst = svr.fetch(n, 'UID')
+    m = UID_RE.match(lst[0])
+    if not m:
+        raise Exception("Internal error parsing UID response: %s %s.  Please try again" % (resp, lst))
+    return m.group(1)
+
 
 def get_message_ctime(d):
-	orig_d = d
-	dt_src = email.utils.parsedate_tz(d)
-	if not dt_src:
-		return None
-	if not dt_src[-1]:
-		# TZ INFO IS MESSY
-		if " --" in d:
-			d = d.replace(" --", " -")
-			dt_src = email.utils.parsedate_tz(d)
-	try:
-		dt = datetime.datetime(*dt_src[:6])
-	except Exception, e:
-		print e
-		print "orig date: %r, curr date: %r, dt_src: %r" % (orig_d, d, dt_src)
-		return None
-	if dt_src[-1]:
-		dt = dt-datetime.timedelta(seconds= dt_src[-1])
-	dt = datetime.datetime.fromtimestamp(calendar.timegm(dt.timetuple()))
-	message_ctime = time.mktime(dt.timetuple())
-	return message_ctime
-	
+    orig_d = d
+    dt_src = email.utils.parsedate_tz(d)
+    if not dt_src:
+        return None
+    if not dt_src[-1]:
+        # TZ INFO IS MESSY
+        if " --" in d:
+            d = d.replace(" --", " -")
+            dt_src = email.utils.parsedate_tz(d)
+    try:
+        dt = datetime.datetime(*dt_src[:6])
+    except Exception, e:
+        print e
+        print "orig date: %r, curr date: %r, dt_src: %r" % (orig_d, d, dt_src)
+        return None
+    if dt_src[-1]:
+        dt = dt-datetime.timedelta(seconds=dt_src[-1])
+    dt = datetime.datetime.fromtimestamp(calendar.timegm(dt.timetuple()))
+    message_ctime = time.mktime(dt.timetuple())
+    return message_ctime
 
 
 def downloadMessage(svr, n, fname):
-	resp, lst = svr.fetch(n, '(RFC822)')
-	if resp!='OK':
-		raise Exception("Bad response: %s %s" % (resp, lst))
-	f = open(fname, 'w')
-	f.write(lst[0][1])
-	f.close()
-	message = email.message_from_string(lst[0][1])
-	message_ctime = get_message_ctime(message['date'])
-	if message_ctime:
-		os.utime(fname, (message_ctime, message_ctime))
-	
+    resp, lst = svr.fetch(n, '(RFC822)')
+    if resp != 'OK':
+        raise Exception("Bad response: %s %s" % (resp, lst))
+    f = open(fname, 'w')
+    f.write(lst[0][1])
+    f.close()
+    message = email.message_from_string(lst[0][1])
+    message_ctime = get_message_ctime(message['date'])
+    if message_ctime:
+        os.utime(fname, (message_ctime, message_ctime))
 
-filere = re.compile(r"(\d+).eml$")
+
 def UIDFromFilename(fname):
-	m = filere.match(fname)
-	if m:
-		return int(m.group(1))
+    m = FILE_RE.match(fname)
+    if m:
+        return int(m.group(1))
 
 
 def get_credentials():
-	user = raw_input("Gmail address: ")
-	pwd = getpass.getpass("Gmail password: ")
-	return user, pwd
-	
+    user = raw_input("Gmail address: ")
+    pwd = getpass.getpass("Gmail password: ")
+    return user, pwd
 
-GMAIL_FOLDER_NAME = "[Gmail]/All Mail"
+
 def do_backup():
-	svr = imaplib.IMAP4_SSL('imap.gmail.com')
-	user, pwd = get_credentials()
-	svr.login(user, pwd)
+    svr = imaplib.IMAP4_SSL('imap.gmail.com')
+    user, pwd = get_credentials()
+    svr.login(user, pwd)
 
-	resp, [countstr] = svr.select(GMAIL_FOLDER_NAME, True)
-	count = int(countstr)
+    resp, [countstr] = svr.select(GMAIL_FOLDER_NAME, True)
+    count = int(countstr)
 
-	existing_files = os.listdir(".")
-	if existing_files:
-		lastdownloaded = max(UIDFromFilename(f) for f in existing_files)
-	else:
-		lastdownloaded = 0
+    existing_files = os.listdir(".")
+    if existing_files:
+        lastdownloaded = max(UIDFromFilename(f) for f in existing_files)
+    else:
+        lastdownloaded = 0
 
+    # A simple binary search to see where we left off
+    gotten, ungotten = 0, count+1
+    while (ungotten-gotten) > 1:
+        attempt = (gotten+ungotten)/2
+        uid = getUIDForMessage(svr, attempt)
+        if int(uid) <= lastdownloaded:
+            print "Finding starting point: %d/%d (UID: %s) too low" % (attempt, count, uid)
+            gotten = attempt
+        else:
+            print "Finding starting point: %d/%d (UID: %s) too high" % (attempt, count, uid)
+            ungotten = attempt
 
-	# A simple binary search to see where we left off
-	gotten, ungotten = 0, count+1
-	while ungotten-gotten>1:
-		attempt = (gotten+ungotten)/2
-		uid = getUIDForMessage(svr, attempt)
-		if int(uid)<=lastdownloaded:
-			print "Finding starting point: %d/%d (UID: %s) too low" % (attempt, count, uid)
-			gotten = attempt
-		else:
-			print "Finding starting point: %d/%d (UID: %s) too high" % (attempt, count, uid)
-			ungotten = attempt
+    # The download loop
+    for i in range(ungotten, count+1):
+        uid = getUIDForMessage(svr, i)
+        print "Downloading %d/%d (UID: %s)" % (i, count, uid)
+        downloadMessage(svr, i, uid+'.eml')
 
+    svr.close()
+    svr.logout()
 
-	# The download loop
-	for i in range(ungotten, count+1):
-		uid = getUIDForMessage(svr, i)
-		print "Downloading %d/%d (UID: %s)" % (i, count, uid)
-		downloadMessage(svr, i, uid+'.eml')
-
-
-	svr.close()
-	svr.logout()
-
-if __name__=="__main__":
-	do_backup()
+if __name__ == "__main__":
+    do_backup()
